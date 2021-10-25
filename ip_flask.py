5e43a924db11923cdef66fe2b56260636ecc64cb
JunaidBantuas <bwabantuasjunaid@gmail.com>
2021-10-25 13:30:26 +0800

    feat: Added flask functions and html basis

--- ip_flask.py
+++ ip_flask.py
@@ -1,10 +1,29 @@
 from flask import *
+from functions import userIP
+from requests import get
+import json
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    pass
+    #<<<<<<<<<<<<<<<<<<<<<<<<------- initialNewdata
+    data = userIP('0.0.0.0')
+    newData = data.json()
+    return render_template('index.html',newData=newData)
+
+@app.route('/API', methods=['POST'])
+def API():
+    if request.method == 'POST':
+        data = userIP(request.form['ipaddress'])
+        newData = data.json()
+    #<<<<<<<<<<<<<<<<<<<<<<<<------- newData handling html
+        if 'error' in newData: 
+            error = True
+            reason = newData['reason']
+            return render_template('index_error.html', error=error, reason=reason)
+        else:
+             return render_template('index.html',newData=newData)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
