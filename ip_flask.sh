3999348af5fea98f638414825a092b6c2afad56a
RNHLD824 <reinholdlucana@gmail.com>
2021-11-11 14:41:17 +0000

    fix: Fix bash script

--- ip_flask.sh
+++ ip_flask.sh
@@ -5,14 +5,19 @@ mkdir tempdir/templates
 mkdir tempdir/static
 
 cp ip_flask.py tempdir/.
+cp ip_api.py tempdir/.
+cp requirements.txt tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-echo "FROM python" >> tempdir/Dockerfile
-echo "RUN pip install flask" >> tempdir/Dockerfile
+
+echo "FROM python" > tempdir/Dockerfile
+echo "COPY ./requirements.txt /home/myapp/" >> tempdir/Dockerfile
+echo "RUN pip install -r /home/myapp/requirements.txt" >> tempdir/Dockerfile
 echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY ip_flask.py /home/myapp/" >> tempdir/Dockerfile
+echo "COPY ./ip_flask.py /home/myapp/" >> tempdir/Dockerfile
+echo "COPY ./ip_api.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
 echo "CMD python3 /home/myapp/ip_flask.py" >> tempdir/Dockerfile
 cd tempdir
